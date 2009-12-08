8625836c7631074f8a27ecd8739d428129374938
Romain Deveaud <romain.deveaud@gmail.com>
2009-12-08 23:49:40 +0100

    abstract robot added

---
+++ lib/robot_abstract.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby
+# Defines the generic robot classes.
+#
+# This module provides the Robot class and RobotListener interface,
+# as well as some helper functions for web requests and responses.
+
+require 'events'
+require 'model'
+require 'ops'
+require 'json'
+require 'util'
+
+class Robot
+end
